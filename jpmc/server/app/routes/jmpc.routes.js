721a7d72152535887865b6221dd49f19cc203b3c
Richa Kulkarni <richakulkarni.98@gmail.com>
2018-07-14 16:08:52 +0530

    wrote server-side APIs

---
+++ jpmc/server/app/routes/jmpc.routes.js
@@ -0,0 +1,25 @@
+const passport = require('../../passport')
+
+module.exports = (app) => {
+    const jpmc = require('../controllers/jpmc.controller.js');
+
+    app.get('/api/jpmc/getsubjects', jpmc.getStudents);
+    app.post('/api/jpmc/addsubject', jpmc.addStudent);
+
+    /*app.post('/api/jpmc/adduser', jpmc.signUpUser);
+    app.get('/api/jpmc/checklogin', jpmc.checkLogIn);
+    app.post('/api/jpmc/login',
+    function (req, res, next) {
+        console.log('routes/user.js, login, req.body: ');
+        console.log(req.body)
+        next()
+    },
+    passport.authenticate('local'),
+    (req, res) => {
+        console.log('logged in', req.user);
+        var userInfo = {
+            username: req.user.username
+        };
+        res.send(userInfo);
+    });*/
+}
