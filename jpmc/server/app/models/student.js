cbea4702fafc960046d78cec04490b31c1e80129
Richa Kulkarni <richakulkarni.98@gmail.com>
2018-07-14 23:10:15 +0530

    Student registration: end to end

--- jpmc/server/app/models/student.js
+++ jpmc/server/app/models/student.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 const Student = mongoose.Schema({
     name: String,
     college: String,
-    disability: String,
+    disability: [String],
     grade: String,
     email: String,
-    phoneno:String,
+    password: String,
+    wphoneno:String,
+    cphoneno:String,
     company:String,
     sector:String,
     skillset:[String],
+    fbid: String,
+    linkedid: String,
+    Address: String,
+    state: String,
+    city: String,
+    pincode: String,
+    resume_url: String,
+    pic_url: String
 });
 
 module.exports = mongoose.model('Student', Student);
